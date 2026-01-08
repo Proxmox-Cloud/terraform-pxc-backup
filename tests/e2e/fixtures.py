35a3f0e9051dcebe6f40db93b659e93663153339
tobi <tobi@tobias-huebner.tech>
2026-01-08 04:57:18 +0000

    Format and cleanup pipeline

--- tests/e2e/fixtures.py
+++ tests/e2e/fixtures.py
@@ -1,117 +1,116 @@
-import pytest
-import yaml
-from proxmoxer import ProxmoxAPI
-import paramiko
-import base64
-import os
-import re
-import random
-import string
-from pve_cloud_test.terraform import apply, destroy
-from kubernetes import client, config
-import tempfile
 import logging
+import os
+import tempfile
+
+import ansible_runner
+import boto3
+import dns.resolver
+import paramiko
+import pytest
 import redis
+import yaml
+from kubernetes import client, config
+from proxmoxer import ProxmoxAPI
 from pve_cloud_test.cloud_fixtures import *
 from pve_cloud_test.k8s_fixtures import *
 from pve_cloud_test.tdd_watchdog import get_ipv4
-import boto3
-import dns.resolver
-import time
-import ansible_runner
-
+from pve_cloud_test.terraform import apply, destroy
 
 logger = logging.getLogger(__name__)
 
+
 @pytest.fixture(scope="session")
 def create_backup_lxc(request, get_proxmoxer, get_test_env):
-  logger.info("test create backup lxc")
+    logger.info("test create backup lxc")
 
-  with tempfile.NamedTemporaryFile('w', suffix='.yaml', delete=False) as temp_dyn_lxcs_inv:
-    yaml.dump({
-      "plugin": "pxc.cloud.lxc_inv",
-
-      "target_pve": get_test_env["pve_test_cluster_name"] + "." + get_test_env["pve_test_cloud_domain"],
-      "stack_name": "pytest-backup-lxc",
-      "lxcs": [
-        {
-          "hostname": "main",
-          "parameters" : {
-            "rootfs": f"volume={get_test_env["pve_test_disk_storage_id"]}:10",
-            "cores": 2,
-            "memory": 1024,
-            "net0": f"name=pve,bridge=vmbr0,firewall=1,ip=dhcp",
-            "mp0": f"volume={get_test_env["pve_test_disk_storage_id"]}:20,mp=/mnt/backup-drive"
-          },
-          "vars": {
-            "PXC_BACKUP_BASE_DIR": "/mnt/backup-drive"
-          }
-        }
-      ],
-      "lxc_global_vars": {
-        "install_prom_systemd_exporter": True
-      },
-      "target_pve_hosts": list(get_test_env["pve_test_hosts"].keys()),
-      "target_pve_hosts": list(get_test_env["pve_test_hosts"].keys()),
-      "root_ssh_pub_key": get_test_env['pve_test_ssh_pub_key']
-    }, temp_dyn_lxcs_inv)
-    temp_dyn_lxcs_inv.flush()
-
-    try:
-      create_lxc_run = ansible_runner.run(
-        project_dir=os.getcwd(),
-        playbook="pxc.cloud.sync_lxcs",
-        inventory=temp_dyn_lxcs_inv.name,
-        verbosity=request.config.getoption("--ansible-verbosity")
-      )
-
-      assert create_lxc_run.rc == 0
-
-      # for local tdd with development watchdogs
-      extra_vars = {}
-      backup_vers, tdd_ip = get_tdd_version("pve-cloud-backup")
-
-      if backup_vers:
-          extra_vars["tdd_local_pypi_host"] = tdd_ip
-          extra_vars["py_pve_cloud_backup_version"] = backup_vers
-
-      setup_bdd_run = ansible_runner.run(
-        project_dir=os.getcwd(),
-        playbook="pxc.cloud.setup_backup_daemon",
-        inventory=temp_dyn_lxcs_inv.name,
-        verbosity=request.config.getoption("--ansible-verbosity"),
-        extravars=extra_vars
-      )
-
-      assert setup_bdd_run.rc == 0
-
-    finally:
-      if not request.config.getoption("--skip-cleanup"):
-        # always run the destroy
-        destroy_lxcs_run = ansible_runner.run(
-          project_dir=os.getcwd(),
-          playbook="playbooks/destroy_lxcs.yaml",
-          inventory=temp_dyn_lxcs_inv.name,
-          verbosity=request.config.getoption("--ansible-verbosity")
+    with tempfile.NamedTemporaryFile(
+        "w", suffix=".yaml", delete=False
+    ) as temp_dyn_lxcs_inv:
+        yaml.dump(
+            {
+                "plugin": "pxc.cloud.lxc_inv",
+                "target_pve": get_test_env["pve_test_cluster_name"]
+                + "."
+                + get_test_env["pve_test_cloud_domain"],
+                "stack_name": "pytest-backup-lxc",
+                "lxcs": [
+                    {
+                        "hostname": "main",
+                        "parameters": {
+                            "rootfs": f"volume={get_test_env["pve_test_disk_storage_id"]}:10",
+                            "cores": 2,
+                            "memory": 1024,
+                            "net0": f"name=pve,bridge=vmbr0,firewall=1,ip=dhcp",
+                            "mp0": f"volume={get_test_env["pve_test_disk_storage_id"]}:20,mp=/mnt/backup-drive",
+                        },
+                        "vars": {"PXC_BACKUP_BASE_DIR": "/mnt/backup-drive"},
+                    }
+                ],
+                "lxc_global_vars": {"install_prom_systemd_exporter": True},
+                "target_pve_hosts": list(get_test_env["pve_test_hosts"].keys()),
+                "target_pve_hosts": list(get_test_env["pve_test_hosts"].keys()),
+                "root_ssh_pub_key": get_test_env["pve_test_ssh_pub_key"],
+            },
+            temp_dyn_lxcs_inv,
         )
-        assert destroy_lxcs_run.rc == 0
+        temp_dyn_lxcs_inv.flush()
 
+        try:
+            create_lxc_run = ansible_runner.run(
+                project_dir=os.getcwd(),
+                playbook="pxc.cloud.sync_lxcs",
+                inventory=temp_dyn_lxcs_inv.name,
+                verbosity=request.config.getoption("--ansible-verbosity"),
+            )
+
+            assert create_lxc_run.rc == 0
+
+            # for local tdd with development watchdogs
+            extra_vars = {}
+            backup_vers, tdd_ip = get_tdd_version("pve-cloud-backup")
+
+            if backup_vers:
+                extra_vars["tdd_local_pypi_host"] = tdd_ip
+                extra_vars["py_pve_cloud_backup_version"] = backup_vers
+
+            setup_bdd_run = ansible_runner.run(
+                project_dir=os.getcwd(),
+                playbook="pxc.cloud.setup_backup_daemon",
+                inventory=temp_dyn_lxcs_inv.name,
+                verbosity=request.config.getoption("--ansible-verbosity"),
+                extravars=extra_vars,
+            )
+
+            assert setup_bdd_run.rc == 0
+
+        finally:
+            if not request.config.getoption("--skip-cleanup"):
+                # always run the destroy
+                destroy_lxcs_run = ansible_runner.run(
+                    project_dir=os.getcwd(),
+                    playbook="playbooks/destroy_lxcs.yaml",
+                    inventory=temp_dyn_lxcs_inv.name,
+                    verbosity=request.config.getoption("--ansible-verbosity"),
+                )
+                assert destroy_lxcs_run.rc == 0
 
 
 @pytest.fixture(scope="session")
 def backup_scenario(request, set_pve_cloud_auth, get_k8s_api_v1, create_backup_lxc):
-  scenario_name = "backup"
+    scenario_name = "backup"
 
-  backup_vers, tdd_ip = get_tdd_version("pve-cloud-backup")
+    backup_vers, tdd_ip = get_tdd_version("pve-cloud-backup")
 
-  if backup_vers:
-      os.environ["TF_VAR_backup_image_base"] = f"{tdd_ip}:5000/pve-cloud-backup"
-      os.environ["TF_VAR_backup_image_version"] = backup_vers
+    if backup_vers:
+        os.environ["TF_VAR_backup_image_base"] = f"{tdd_ip}:5000/pve-cloud-backup"
+        os.environ["TF_VAR_backup_image_version"] = backup_vers
 
-  if not request.config.getoption("--skip-apply"):
-    apply("pxc-backup", scenario_name, get_k8s_api_v1, True, True) # this will wait till everything is running after apply
+    if not request.config.getoption("--skip-apply"):
+        apply(
+            "pxc-backup", scenario_name, get_k8s_api_v1, True, True
+        )  # this will wait till everything is running after apply
 
-  yield 
+    yield
 
-  if not request.config.getoption("--skip-cleanup"):
-    destroy(scenario_name)
+    if not request.config.getoption("--skip-cleanup"):
+        destroy(scenario_name)
--- tests/e2e/test_backup.py
+++ tests/e2e/test_backup.py
@@ -1,232 +1,235 @@
-
-from fixtures import *
-import ssl
-import socket
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
-from cryptography.x509.oid import NameOID
+import asyncio
+import logging
+import pickle
+import random
+import string
+import struct
 import time
-from kubernetes import client
+
 import dns.query
 import dns.tsigkeyring
 import dns.zone
-import logging
-import requests
-import pprint
-import random
-import string
-from kubernetes.stream import stream
-from kubernetes.client import V1ObjectMeta, V1Job, V1JobSpec
-import json
-import requests
-from kubernetes.client.rest import ApiException
 import pytest
-from pve_cloud_backup.daemon.brctl import launch_restore_job, get_parser
-from types import SimpleNamespace
-import asyncio
+import requests
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.x509.oid import NameOID
+from fixtures import *
+from kubernetes import client
+from kubernetes.client import V1Job, V1JobSpec, V1ObjectMeta
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
+from pve_cloud_backup.daemon.brctl import get_parser, launch_restore_job
 from pve_cloud_backup.daemon.rpc import Command
-import struct
-import pickle
 
 logger = logging.getLogger(__name__)
 
 
 def random_string(length=16):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
 @pytest.mark.asyncio
 async def test_backup(get_test_env, get_proxmoxer, set_k8s_auth, backup_scenario):
-  logger.info("test backup create and restore")
+    logger.info("test backup create and restore")
 
-  kubeconfig = set_k8s_auth
+    kubeconfig = set_k8s_auth
 
-  # auth kubernetes api
-  with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
-    temp_file.write(kubeconfig)
-    temp_file.flush()
-    config.load_kube_config(config_file=temp_file.name)
+    # auth kubernetes api
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
+        temp_file.write(kubeconfig)
+        temp_file.flush()
+        config.load_kube_config(config_file=temp_file.name)
 
-  v1 = client.CoreV1Api()
-  v1_batch = client.BatchV1Api()
+    v1 = client.CoreV1Api()
+    v1_batch = client.BatchV1Api()
 
-  # create random file with random content in test pod
-  pods = v1.list_namespaced_pod(namespace="test-backup-source")
+    # create random file with random content in test pod
+    pods = v1.list_namespaced_pod(namespace="test-backup-source")
 
-  assert pods.items
+    assert pods.items
 
-  pod_name = pods.items[0].metadata.name
-  
-  filename = f"/mnt/data/{random_string(12)}.txt"
-  content = random_string(32)
+    pod_name = pods.items[0].metadata.name
 
-  resp = stream(
-    v1.connect_get_namespaced_pod_exec,
-    pod_name,
-    "test-backup-source",
-    command=[
-      "sh",
-      "-c",
-      f"echo '{content}' > {filename}"
-    ],
-    stderr=True,
-    stdin=False,
-    stdout=True,
-    tty=False
-  )
+    filename = f"/mnt/data/{random_string(12)}.txt"
+    content = random_string(32)
 
-  # trigger the backup cron and monitor
-  cronjob_name = "fetcher-cron"
-  cj = v1_batch.read_namespaced_cron_job(name=cronjob_name, namespace="pve-cloud-backup")
-  tmpl = cj.spec.job_template
-  job_name = f"{cronjob_name}-manual-{int(time.time())}"
+    resp = stream(
+        v1.connect_get_namespaced_pod_exec,
+        pod_name,
+        "test-backup-source",
+        command=["sh", "-c", f"echo '{content}' > {filename}"],
+        stderr=True,
+        stdin=False,
+        stdout=True,
+        tty=False,
+    )
 
-  print(f"launching {job_name}")
+    # trigger the backup cron and monitor
+    cronjob_name = "fetcher-cron"
+    cj = v1_batch.read_namespaced_cron_job(
+        name=cronjob_name, namespace="pve-cloud-backup"
+    )
+    tmpl = cj.spec.job_template
+    job_name = f"{cronjob_name}-manual-{int(time.time())}"
 
-  job = V1Job(
-      metadata=V1ObjectMeta(name=job_name),
-      spec=V1JobSpec(
-          template=tmpl.spec.template,
-          backoff_limit=tmpl.spec.backoff_limit,
-      ),
-  )
+    print(f"launching {job_name}")
 
-  job = v1_batch.create_namespaced_job(namespace="pve-cloud-backup", body=job)
+    job = V1Job(
+        metadata=V1ObjectMeta(name=job_name),
+        spec=V1JobSpec(
+            template=tmpl.spec.template,
+            backoff_limit=tmpl.spec.backoff_limit,
+        ),
+    )
 
-  while True:
-    time.sleep(5) # give pods some time to create / dont spam api
+    job = v1_batch.create_namespaced_job(namespace="pve-cloud-backup", body=job)
 
-    # fetch the pod and wait for it to finish
-    pods = v1.list_namespaced_pod(
-      namespace="pve-cloud-backup",
-      label_selector=f"job-name={job_name}"
-    ).items
+    while True:
+        time.sleep(5)  # give pods some time to create / dont spam api
 
-    assert pods
+        # fetch the pod and wait for it to finish
+        pods = v1.list_namespaced_pod(
+            namespace="pve-cloud-backup", label_selector=f"job-name={job_name}"
+        ).items
 
-    pod = pods[0]
+        assert pods
 
-    phase = pod.status.phase
+        pod = pods[0]
 
-    assert phase != "Failed", f"pod {pod.metadata.name} failed!" # failed pods end tests immediatly
+        phase = pod.status.phase
 
-    if phase == "Succeeded":
-      break # finished
+        assert (
+            phase != "Failed"
+        ), f"pod {pod.metadata.name} failed!"  # failed pods end tests immediatly
 
-    logger.info(f"pod {pod.metadata.name} in phase {phase}")
+        if phase == "Succeeded":
+            break  # finished
 
+        logger.info(f"pod {pod.metadata.name} in phase {phase}")
 
-  # find the backup lxc, get its ip and paramiko into it to test the if the backup was created
-  backup_lxc = None
-  for node in get_proxmoxer.nodes.get():
-    for lxc in get_proxmoxer.nodes(node["node"]).lxc.get():
-      if "main-pytest-backup-lxc" in lxc["name"]:
-        backup_lxc = lxc
+    # find the backup lxc, get its ip and paramiko into it to test the if the backup was created
+    backup_lxc = None
+    for node in get_proxmoxer.nodes.get():
+        for lxc in get_proxmoxer.nodes(node["node"]).lxc.get():
+            if "main-pytest-backup-lxc" in lxc["name"]:
+                backup_lxc = lxc
 
-  assert backup_lxc
+    assert backup_lxc
 
-  logger.info(backup_lxc)
+    logger.info(backup_lxc)
 
-  resolver = dns.resolver.Resolver()
-  resolver.nameservers = [get_test_env['pve_test_cloud_inv']['bind_master_ip']]
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = [get_test_env["pve_test_cloud_inv"]["bind_master_ip"]]
 
-  ddns_answer = resolver.resolve(f"{backup_lxc['name']}.{get_test_env['pve_test_cloud_domain']}")
-  ddns_ips = [rdata.to_text() for rdata in ddns_answer]
-  logger.info(ddns_ips)
-  assert ddns_ips # assert ddns response
+    ddns_answer = resolver.resolve(
+        f"{backup_lxc['name']}.{get_test_env['pve_test_cloud_domain']}"
+    )
+    ddns_ips = [rdata.to_text() for rdata in ddns_answer]
+    logger.info(ddns_ips)
+    assert ddns_ips  # assert ddns response
 
-  time.sleep(10) # wait for borg repo lock to be released
+    time.sleep(10)  # wait for borg repo lock to be released
 
-  # call brctl methods
-  reader, writer = await asyncio.open_connection(ddns_ips[0], 8085)
-  writer.write(struct.pack("B", Command.LIST_BACKUPS.value))
-  await writer.drain()
+    # call brctl methods
+    reader, writer = await asyncio.open_connection(ddns_ips[0], 8085)
+    writer.write(struct.pack("B", Command.LIST_BACKUPS.value))
+    await writer.drain()
 
-  # read the response archives size and then the archives
-  dict_size = struct.unpack('!I', (await reader.readexactly(4)))[0]
-  archives = pickle.loads((await reader.readexactly(dict_size)))
+    # read the response archives size and then the archives
+    dict_size = struct.unpack("!I", (await reader.readexactly(4)))[0]
+    archives = pickle.loads((await reader.readexactly(dict_size)))
 
-  # fetch local backup image version if build via tdd
-  backup_vers, tdd_ip = get_tdd_version("pve-cloud-backup")
+    # fetch local backup image version if build via tdd
+    backup_vers, tdd_ip = get_tdd_version("pve-cloud-backup")
 
-  image = None
-  if backup_vers:
-      image = f"{tdd_ip}:5000/pve-cloud-backup:{backup_vers}"
+    image = None
+    if backup_vers:
+        image = f"{tdd_ip}:5000/pve-cloud-backup:{backup_vers}"
 
-  latest_timestamp = sorted(archives)[-1]
-  logger.info(latest_timestamp)
+    latest_timestamp = sorted(archives)[-1]
+    logger.info(latest_timestamp)
 
-  brctl_parser = get_parser()
+    brctl_parser = get_parser()
 
-  restore_args = brctl_parser.parse_args([
-    "restore-k8s",
-    "--bdd-host", ddns_ips[0],
-    "--target-pve", f"{get_test_env['pve_test_cluster_name']}.{get_test_env['pve_test_cloud_domain']}",
-    "--stack-name", "pytest-k8s",
-    "--image", image,
-    "--timestamp", latest_timestamp,
-    "--namespace-mapping", "test-backup-source:test-backup-restore",
-    "--auto-scale", "--auto-delete"
-  ])
+    restore_args = brctl_parser.parse_args(
+        [
+            "restore-k8s",
+            "--bdd-host",
+            ddns_ips[0],
+            "--target-pve",
+            f"{get_test_env['pve_test_cluster_name']}.{get_test_env['pve_test_cloud_domain']}",
+            "--stack-name",
+            "pytest-k8s",
+            "--image",
+            image,
+            "--timestamp",
+            latest_timestamp,
+            "--namespace-mapping",
+            "test-backup-source:test-backup-restore",
+            "--auto-scale",
+            "--auto-delete",
+        ]
+    )
 
-  launch_restore_job(restore_args)
+    launch_restore_job(restore_args)
 
+    # wait for the restore job to finish
+    while True:
+        time.sleep(5)  # give pods some time to create / dont spam api
 
-  # wait for the restore job to finish
-  while True:
-    time.sleep(5) # give pods some time to create / dont spam api
+        # fetch the pod and wait for it to finish
+        pods = v1.list_namespaced_pod(
+            namespace="pve-cloud-backup",
+            label_selector=f"job=pxc-restore-{latest_timestamp}",
+        ).items
 
-    # fetch the pod and wait for it to finish
-    pods = v1.list_namespaced_pod(
-      namespace="pve-cloud-backup",
-      label_selector=f"job=pxc-restore-{latest_timestamp}"
-    ).items
+        assert pods
 
-    assert pods
+        pod = pods[0]
 
-    pod = pods[0]
+        phase = pod.status.phase
 
-    phase = pod.status.phase
+        assert (
+            phase != "Failed"
+        ), f"pod {pod.metadata.name} failed!"  # failed pods end tests immediatly
 
-    assert phase != "Failed", f"pod {pod.metadata.name} failed!" # failed pods end tests immediatly
+        if phase == "Succeeded":
+            break  # finished
 
-    if phase == "Succeeded":
-      break # finished
+        logger.info(f"pod {pod.metadata.name} in phase {phase}")
 
-    logger.info(f"pod {pod.metadata.name} in phase {phase}")
+    # wait for the pod to be running again and exec into it when it is
+    while True:
+        pods = v1.list_namespaced_pod(
+            namespace="test-backup-restore", label_selector=f"app=busybox"
+        ).items
 
-  # wait for the pod to be running again and exec into it when it is
-  while True:
-    pods = v1.list_namespaced_pod(
-      namespace="test-backup-restore",
-      label_selector=f"app=busybox"
-    ).items
+        assert pods
 
-    assert pods
+        pod = pods[0]
 
-    pod = pods[0]
+        phase = pod.status.phase
 
-    phase = pod.status.phase
+        assert (
+            phase != "Failed"
+        ), f"pod {pod.metadata.name} failed!"  # failed pods end tests immediatly
 
-    assert phase != "Failed", f"pod {pod.metadata.name} failed!" # failed pods end tests immediatly
+        if phase == "Running":
+            time.sleep(5)  # give some small time buffer to init fully
+            break  # ready for exec
 
-    if phase == "Running":
-      time.sleep(5) # give some small time buffer to init fully
-      break # ready for exec
+    resp = stream(
+        v1.connect_get_namespaced_pod_exec,
+        pod.metadata.name,
+        "test-backup-restore",
+        command=["cat", filename],
+        stderr=True,
+        stdin=False,
+        stdout=True,
+        tty=False,
+    )
 
-  resp = stream(
-    v1.connect_get_namespaced_pod_exec,
-    pod.metadata.name,
-    "test-backup-restore",
-    command=[
-      "cat", filename
-    ],
-    stderr=True,
-    stdin=False,
-    stdout=True,
-    tty=False
-  )
+    logger.info(resp)
 
-  logger.info(resp)
-
-  assert resp.strip() == content
+    assert resp.strip() == content
