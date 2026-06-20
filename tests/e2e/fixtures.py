629124e2e7d67ec49fa323ce57069334c43ef3da
tobi <tobi_h@outlook.com>
2026-06-20 00:10:44 +0000

    update tests

--- tests/e2e/fixtures.py
+++ tests/e2e/fixtures.py
@@ -2,7 +2,6 @@ import logging
 import os
 import tempfile
 
-import ansible_runner
 import boto3
 import dns.resolver
 import paramiko
@@ -54,19 +53,6 @@ def create_backup_lxc(request, get_proxmoxer, get_test_env):
         )
         temp_dyn_lxcs_inv.flush()
 
-        # we have to prefix the full path otherwise ansible-runner might ignore and use pxc collection from default path
-        # this is a bug/error inside ansible-runner since it should honor the default ANSIBLE_COLLECTIONS_PATH env variable
-        logger.info(f"collections path {os.getenv('ANSIBLE_COLLECTIONS_PATH')}")
-        create_lxc_run = ansible_runner.run(
-            project_dir=os.getcwd(),
-            playbook=f"{os.getenv('ANSIBLE_COLLECTIONS_PATH')}/ansible_collections/pxc/cloud/playbooks/sync_lxcs.yaml",
-            inventory=temp_dyn_lxcs_inv.name,
-            verbosity=request.config.getoption("--ansible-verbosity"),
-        )
-
-        assert create_lxc_run.rc == 0
-
-        # for local tdd with development watchdogs
         extra_vars = {}
         backup_vers, tdd_ip = get_tdd_version("pve-cloud-backup")
 
@@ -74,26 +60,14 @@ def create_backup_lxc(request, get_proxmoxer, get_test_env):
             extra_vars["tdd_local_pypi_host"] = tdd_ip
             extra_vars["py_pve_cloud_backup_version"] = backup_vers
 
-        setup_bdd_run = ansible_runner.run(
-            project_dir=os.getcwd(),
-            playbook=f"{os.getenv('ANSIBLE_COLLECTIONS_PATH')}/ansible_collections/pxc/cloud/playbooks/setup_backup_daemon.yaml",
-            inventory=temp_dyn_lxcs_inv.name,
-            verbosity=request.config.getoption("--ansible-verbosity"),
-            extravars=extra_vars,
-        )
-
-        assert setup_bdd_run.rc == 0
-
-        yield
-
-        # always run the destroy
-        destroy_lxcs_run = ansible_runner.run(
-            project_dir=os.getcwd(),
-            playbook=f"{os.getenv('ANSIBLE_COLLECTIONS_PATH')}/ansible_collections/pxc/cloud/playbooks/destroy_lxcs.yaml",
-            inventory=temp_dyn_lxcs_inv.name,
-            verbosity=request.config.getoption("--ansible-verbosity"),
-        )
-        assert destroy_lxcs_run.rc == 0
+        collections_path = os.getenv('ANSIBLE_COLLECTIONS_PATH')
+        with run_playbook(request, temp_dyn_lxcs_inv.name,
+            f"{collections_path}/ansible_collections/pxc/cloud/playbooks/sync_lxcs.yaml",
+            f"{collections_path}/ansible_collections/pxc/cloud/playbooks/setup_backup_daemon.yaml",
+            destroy_playbook=f"{collections_path}/ansible_collections/pxc/cloud/playbooks/destroy_lxcs.yaml",
+            extra_vars=extra_vars
+        ):
+            yield
 
 
 @cloud_fixture("scenario")
