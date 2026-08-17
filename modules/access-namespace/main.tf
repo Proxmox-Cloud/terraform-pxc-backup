0b6688e26c47b6cde91698b09aa9f0bc207d8094
tobi <tobi_h@outlook.com>
2026-08-17 14:41:09 +0000

    restore optiosn

--- modules/access-namespace/main.tf
+++ modules/access-namespace/main.tf
@@ -59,3 +59,53 @@ resource "kubernetes_secret" "ceph_secrets" {
     "ceph-admin-keyring" = data.pxc_ceph_access.ceph_access[0].admin_keyring
   }
 }
+
+variable "deploy_restore_secrets" {
+  type = string
+  description = <<-EOF
+    Use this variable to trigger creation needed for restoring backups. This is useful for restore only options where you
+    dont want to configure a backup job.
+  EOF
+  default = null
+  validation {
+    condition = var.deploy_restore_secrets == null || contains(
+      ["kubespray", "k0s-edge"], var.deploy_restore_secrets
+    )
+    error_message = "Deploy restore secrets options are kubespray, k0s-edge or null!"
+  }
+}
+
+
+data "pxc_ssh_key" "automation" {
+  count = var.deploy_restore_secrets == "kubespray" ? 1 : 0
+  key_type = "AUTOMATION"
+}
+
+data "pxc_cloud_self" "self" {}
+
+locals {
+  cluster_vars = yamldecode(data.pxc_cloud_self.self.cluster_vars)
+
+  k8s_stack_fqdn = "${data.pxc_cloud_self.self.stack_name}.${local.cluster_vars.pve_cloud_domain}"
+}
+
+# ssh key to access the k0s hosts
+data "pxc_cloud_secret" "k0s_edge_host_key" {
+  count = var.deploy_restore_secrets == "k0s-edge" ? 1 : 0
+  secret_name = "${local.k8s_stack_fqdn}-k0s-edge-key"
+}
+
+resource "kubernetes_secret" "fetcher_secrets" {
+  metadata {
+    name = "fetcher-secrets"
+    namespace = kubernetes_namespace.backup.metadata[0].name
+  }
+  data = merge(
+    var.deploy_restore_secrets == "kubespray" ? {
+      "qemu-id" = data.pxc_ssh_key.automation[0].key
+    } : {},
+    var.deploy_restore_secrets == "k0s_edge" ? {
+      "ext-id" = jsondecode(data.pxc_cloud_secret.k0s_edge_host_key[0].secret_data)["id_ed25519"]
+    } : {}
+  )
+}
